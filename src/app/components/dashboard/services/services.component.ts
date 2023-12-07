dc560da5c3bd66954b06ec58475c452ffe2856df
Dori66 <dorantdushi@gmail.com>
2023-12-07 20:37:47 +0100

    smart text changes

--- src/app/components/dashboard/services/services.component.ts
+++ src/app/components/dashboard/services/services.component.ts
@@ -10,22 +10,22 @@ export class ServicesComponent {
   public services = [
     {
       title: 'Web Development',
-      description: 'Crafting beautiful and responsive websites tailored to your business needs. Our expert developers utilize the latest technologies to create engaging online experiences that captivate your audience.',
+      description: 'Crafting beautiful and responsive websites tailored to your business needs.Nexiphor utilize the latest technologies to create engaging online experiences that captivate your audience.',
       icon: 'globe'
     },
     {
       title: 'Mobile App Development',
-      description: 'Transforming ideas into powerful mobile applications for both iOS and Android platforms. We bring innovation and functionality together to deliver seamless and feature-rich apps that stand out in the app stores.',
+      description: 'Transforming ideas into powerful mobile applications for both iOS and Android platforms. Nexiphor brings innovation and functionality together to deliver seamless and feature-rich apps that stand out in the app stores.',
       icon: 'tablet'
     },
     {
       title: 'UI/UX Design',
-      description: 'Bringing user-centered design to the forefront, we create interfaces that are not just visually appealing but also intuitive and user-friendly. Our designs ensure a smooth and delightful user experience.',
+      description: 'Bringing user-centered design to the forefront, Nexuphor creates interfaces that are not just visually appealing but also intuitive and user-friendly. Our designs ensure a smooth and delightful user experience.',
       icon: 'magic'
     },
     {
       title: 'Custom Software Solutions',
-      description: 'Tailoring software solutions to address your specific business challenges. Our team of experts collaborates closely with you to develop customized software that streamlines processes and boosts efficiency.',
+      description: 'Tailoring software solutions to address your specific business challenges.Our team of experts collaborates closely with you to develop customized software that streamlines processes and boosts efficiency.',
       icon: 'cogs'
     },
     {
